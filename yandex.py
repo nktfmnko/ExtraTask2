e619b47637fd5ae7fcd923af867bd40acd1f6dcb
nktfmnko <nktfmnko@gmail.com>
2023-12-12 18:27:36 +0300

    connect to yandex tracker

---
+++ yandex.py
@@ -0,0 +1,15 @@
+from yandex_tracker_client import TrackerClient
+
+file = open("id.txt")
+token = file.readline().strip()
+org_id = file.readline().strip()
+
+client = TrackerClient(token=token, cloud_org_id=org_id)
+
+issues = client.issues.get_all()
+last_issue = str(issues[0])
+try:
+    client.issues.create(queue='TEAMCITYBUILDFA', summary=f"Issue № {last_issue[len(last_issue) - 2]}",
+                         type={'name': 'Bug'})
+except Exception as ex:
+    print("exception:\n", ex)
